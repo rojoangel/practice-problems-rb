92c0a5cd914b521005f9ddbdca97b3aa5a964670
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-10-12 21:48:41 +0200

    ignore punctuation

--- week8/section-8-solution.rb
+++ week8/section-8-solution.rb
@@ -7,7 +7,16 @@ class ExtendedGuessTheWordGame < GuessTheWordGame
 end
 
 class ExtendedSecretWord < SecretWord
-  ## YOUR CODE HERE
+  attr_accessor :word, :pattern
+
+  def initialize word
+    self.word = word
+    self.pattern = initial_pattern word
+  end
+
+  def initial_pattern word
+    (word.chars.map {|ch| if ch.match(/^[[:alpha:]]$/) then '-' else ch end}).join
+  end
 end
 
 ## Change to `false` to run the original game
